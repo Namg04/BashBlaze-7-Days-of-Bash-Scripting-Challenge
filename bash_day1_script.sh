d234cb68b7b1e43cddee25207a0d74d6c32e192b
Ubuntu <ubuntu@ip-172-31-30-111.ec2.internal>
2023-07-31 14:08:18 +0000

    Task day1 completed

---
+++ bash_day1_script.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+#Task_01: comment
+
+#This line is a comment in shell scripting. Any text following hash symbol on the same line will be treated as a comment.
+
+#Task_02 : Echo
+
+echo "Excited to start this shell script  challenge with TWS_batch_4"
+
+#Task_03 : Variable
+
+var1="Day1"
+var2="of_shell_scripting"
+
+#Task_04 :Using Variables
+
+hello="Today is $var1 $var2 challenge initiated by TWS community.
+echo "$hello
+
+#Task_05 : Using Built-in Variables
+
+echo "Home directory : $HOME"
+echo "Username : $USER"
+echo "Default shell : $SHELL"
+echo "Current bash path : $BASH"
+echo "My hostname - $HOSTNAME"
+
+#Task_06 : Wildcards
+
+echo "Files with .sh extension in the current directory:"
+ls *.sh
